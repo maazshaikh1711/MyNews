525028cebf7b47acffca9fc064bfffac022f8afd
maazshaikh1711 <0maaz0shaikh0@gmail.com>
2022-01-13 22:41:24 +0530

    added card swiping feature

--- App.js
+++ App.js
@@ -1,112 +1,158 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+'use strict';
+ 
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet, Text, View, Image, LogBox} from 'react-native';
+import SwipeCards from 'react-native-swipe-cards';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+LogBox.ignoreLogs(["Animated"]);
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+ 
+  render() {
+    return (
+      <View style={styles.card}>
+        <Image style={styles.thumbnail} source={{uri: this.props.image}} />
+        {/* <Text style={styles.text}>This is card {this.props.name}</Text> */}
+        <View style={{height: 70, paddingTop: 5}}>
+          <Text style={styles.title}>Lost puppy, if found please return kjbdzvksjsdzbvkjnv</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
+        <View style={{margin: '5%'}}>
+          <Text style={styles.text}>Lost puppy, if found please return kjbdzvk sjsdzbv kjnv gyihrfsjldk irufsvnj mrisgfbvkj cnriusfbkjvcx , Lost puppy, if found please return kjbdzvk sjsdzbv kjnv gyihrfsjldk irufsvnj mrisgfbvkj cnriusfbkjvcx Lost puppy, if found please return kjbdzvk sjsdzbv kjnv gyihrfsjldk irufsvnj mrisgfbvkj cnriusfbkjvcx Lost puppy, if found please return kjbdzvk sjsdzbv kjnv gyihrfsjldk irufsvnj mrisgfbvkj cnriusfbkjvcx Lost puppy, if found please return kjbdzvk sjsdzbv kjnv gyihrfsjldk irufsvnj mrisgfbvkj cnriusfbkjvcx </Text>
+        </View>
+      </View>
+    )
+  }
+}
+ 
+class NoMoreCards extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+ 
+  render() {
+    return (
+      <View style={styles.noMoreCards}>
+        <Text>No more cards</Text>
+      </View>
+    )
+  }
+}
+ 
+const cards = [
+  {name: '1', image: 'https://media.giphy.com/media/GfXFVHUzjlbOg/giphy.gif'},
+  {name: '2', image: 'https://media.giphy.com/media/irTuv1L1T34TC/giphy.gif'},
+  {name: '3', image: 'https://media.giphy.com/media/LkLL0HJerdXMI/giphy.gif'},
+  {name: '4', image: 'https://media.giphy.com/media/fFBmUMzFL5zRS/giphy.gif'},
+  {name: '5', image: 'https://media.giphy.com/media/oDLDbBgf0dkis/giphy.gif'},
+  {name: '6', image: 'https://media.giphy.com/media/7r4g8V2UkBUcw/giphy.gif'},
+  {name: '7', image: 'https://media.giphy.com/media/K6Q7ZCdLy8pCE/giphy.gif'},
+  {name: '8', image: 'https://media.giphy.com/media/hEwST9KM0UGti/giphy.gif'},
+  {name: '9', image: 'https://media.giphy.com/media/3oEduJbDtIuA2VrtS0/giphy.gif'},
+]
+ 
+const cards2 = [
+  {name: '10', image: 'https://media.giphy.com/media/12b3E4U9aSndxC/giphy.gif'},
+  {name: '11', image: 'https://media4.giphy.com/media/6csVEPEmHWhWg/200.gif'},
+  {name: '12', image: 'https://media4.giphy.com/media/AA69fOAMCPa4o/200.gif'},
+  {name: '13', image: 'https://media.giphy.com/media/OVHFny0I7njuU/giphy.gif'},
+]
+ 
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: cards,
+      outOfCards: false
+    }
+  }
+ 
+  handleYup (card) {
+    console.log("yup")
+  }
+ 
+  handleNope (card) {
+    console.log("nope")
+  }
+ 
+  cardRemoved (index) {
+    console.log(`The index is ${index}`);
+ 
+    let CARD_REFRESH_LIMIT = 3
+ 
+    if (this.state.cards.length - index <= CARD_REFRESH_LIMIT + 1) {
+      console.log(`There are only ${this.state.cards.length - index - 1} cards left.`);
+ 
+      if (!this.state.outOfCards) {
+        console.log(`Adding ${cards2.length} more cards`)
+ 
+        this.setState({
+          cards: this.state.cards.concat(cards2),
+          outOfCards: true
+        })
+      }
+ 
+    }
+ 
+  }
+ 
+  render() {
+    return (
+      <View style={{flex:1, justifyContent: 'center', alignItems: 'center',backgroundColor:"#192734"}}>
+      
+        <SwipeCards
+          cards={this.state.cards}
+          loop={false}
+  
+          renderCard={(cardData) => <Card {...cardData} />}
+          renderNoMoreCards={() => <NoMoreCards />}
+          showYup={true}
+          showNope={true}
+  
+          handleYup={this.handleYup}
+          handleNope={this.handleNope}
+          cardRemoved={this.cardRemoved.bind(this)}
+        />
+      </View>
+    )
+  }
+}
+ 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  card: {
+    alignItems: 'center',
+    borderRadius: 5,
+    overflow: 'hidden',
+    borderColor: 'grey',
+    backgroundColor: '#3A3B3C',
+    borderWidth: 1,
+    elevation: 1,
+    height: '85%',
+    width: '90%',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
-export default App;
+  title: {
+    // flex: 1, 
+    // flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    fontSize: 25,
+    fontWeight: 'bold',
+    fontColor: '#0000',
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    marginTop: 10,
+  },
+  text: {
+    fontSize: 20,
+    // paddingTop: 10px,
+  },
+  noMoreCards: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
