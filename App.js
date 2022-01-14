59ed65a20caf61c4b5134af6b573a4f22661b12a
maazshaikh1711 <0maaz0shaikh0@gmail.com>
2022-01-15 01:37:53 +0530

    added bottom tab bars

--- App.js
+++ App.js
@@ -1,18 +1,63 @@
 'use strict';
  
 import React from 'react';
-import {StyleSheet, Text, View, LogBox} from 'react-native';
-import CardNews from './src/CardNews';
+import {StyleSheet, Text, View, LogBox, StatusBar} from 'react-native';
 import {Header} from './src/common/components/Header';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import ForYouNews from './src/ForYouNewsPage/ForYouNews';
+import AllNews from './src/AllNewsPage/AllNews';
 
 LogBox.ignoreLogs(["Animated"]);
 LogBox.ignoreLogs(["ComponentWillReceiveProps has been"]);
 
+export const Tabbars = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'For You') {
+            iconName = focused ? 'md-home-sharp' : 'md-home-outline';
+          } else if (route.name === 'All News') {
+            iconName = focused? 'stats-chart' : 'stats-chart-outline';
+          }
+          
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+
+      screenOptions = {{
+        headerShown: false
+      }}
+      
+      tabBarOptions={{
+        activeTintColor: 'white',
+        inactiveTintColor: 'black',
+        activeBackgroundColor: '#0D47A1',
+        inactiveBackgroundColor: '#1465e3',
+        showLabel: true,
+      }}>
+      <Tab.Screen name="For You" component={ForYouNews} />
+      <Tab.Screen name="All News" component={AllNews} />
+
+    </Tab.Navigator>
+  );
+};
+
 const App = () => {
   return (
     <>
-      <Header title={"Rapid News"}/>
-      <CardNews/>
+      <StatusBar barStyle="dark-content" backgroundColor="#0D47A1" />
+      <NavigationContainer documentTitle={{ enabled : false }} >
+        <Header title={"Rapid News"}/>
+        <Tabbars/>
+        {/* <CardNews/> */}
+      </NavigationContainer>
     </>
   )
 }
