2486bafe40470b98f94d24380de0cec44cb9472c
maazshaikh1711 <0maaz0shaikh0@gmail.com>
2022-01-14 10:49:43 +0530

    added a module for dot-env

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,13 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'
             // , 'module:react-native-dotenv"
             ],
+  plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
+  ],
 };
