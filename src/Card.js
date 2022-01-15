185733b7e388008e81c227eac5f006aeeba17654
maazshaikh1711 <0maaz0shaikh0@gmail.com>
2022-01-15 13:38:24 +0530

    converted to fnctionl n added modal onPress card

--- src/Card.js
+++ src/Card.js
@@ -1,28 +1,50 @@
-import React from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Image, TouchableOpacity, Modal, Alert} from 'react-native';
 
-export default class Card extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-   
-    render() {
+export const Card = ({props}) => {
+
+  const [modalVisible, setModalVisible] = useState(false);
+  
       return (
-        <View style={styles.card}>
-  
-          <Image style={styles.thumbnail} source={{uri: this.props.urlToImage}} />
-          
-          <View style={{height: 70, paddingTop: 5}}>
-            <Text style={styles.title}>{this.props.title}</Text>
+        <>
+        <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center",}}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <TouchableOpacity style={styles.button}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text>Hide Modal</Text>
+            </TouchableOpacity>
           </View>
-          
-          <View style={{margin: '5%'}}>
-            <Text style={styles.text}>{this.props.description}</Text>
-          </View>
-  
         </View>
+      </Modal>
+
+          <TouchableOpacity style={{ alignItems: 'center' , justifyContent: 'center' }} onPress={()=> setModalVisible(true)}>
+            <View style={styles.card}>
+      
+              <Image style={styles.thumbnail} source={{uri: props.urlToImage}} />
+              
+              <View style={{height: 70, paddingTop: 5}}>
+                <Text style={styles.title}>{props.title}</Text>
+              </View>
+              
+              <View style={{margin: '5%'}}>
+                <Text style={styles.text}>{props.description}</Text>
+              </View>
+      
+            </View>
+          </TouchableOpacity>
+        </>
       )
-    }
 }
 
 const styles = StyleSheet.create({
@@ -51,4 +73,30 @@ const styles = StyleSheet.create({
     text: {
       fontSize: 20,
     },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center"
+    },
+    button: {
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2
+    },
 })
+
+export default Card;
