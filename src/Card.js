3a96d6a45dd404d76af465564c7899afb3667485
maazshaikh1711 <0maaz0shaikh0@gmail.com>
2022-01-16 00:20:40 +0530

    shifted modal component to new file

--- src/Card.js
+++ src/Card.js
@@ -1,34 +1,23 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, View, Image, TouchableOpacity, Modal, Alert} from 'react-native';
+import ShowArticleModal from './common/ShowArticleModal';
 
 export const Card = ({props}) => {
 
-  const [modalVisible, setModalVisible] = useState(false);
-  
+   const [openArticleModal, setOpenArticalModal]= useState(false)
+
       return (
         <>
-        <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={{flex: 1, justifyContent: "center", alignItems: "center",}}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <TouchableOpacity style={styles.button}
-              onPress={() => setModalVisible(!modalVisible)}
-            >
-              <Text>Hide Modal</Text>
-            </TouchableOpacity>
-          </View>
+        <View>
+          {openArticleModal && <ShowArticleModal onClose={()=>{setOpenArticalModal(false)}}/>
+          }
         </View>
-      </Modal>
-
-          <TouchableOpacity style={{ alignItems: 'center' , justifyContent: 'center' }} onPress={()=> setModalVisible(true)}>
+          <TouchableOpacity style={{ alignItems: 'center' , justifyContent: 'center' }} onPress={()=> 
+            setOpenArticalModal(true)
+          // <ShowArticleModal visible={true}/> 
+              // {1==1 && <ShowArticleModal visible={true}/>}
+              // Alert.alert("Tapped on a card")
+          }>
             <View style={styles.card}>
       
               <Image style={styles.thumbnail} source={{uri: props.urlToImage}} />
@@ -43,7 +32,7 @@ export const Card = ({props}) => {
       
             </View>
           </TouchableOpacity>
-        </>
+          </>
       )
 }
 
@@ -72,30 +61,6 @@ const styles = StyleSheet.create({
     },
     text: {
       fontSize: 20,
-    },
-    modalView: {
-      margin: 20,
-      backgroundColor: "white",
-      borderRadius: 20,
-      padding: 35,
-      alignItems: "center",
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 4,
-      elevation: 5
-    },
-    modalText: {
-      marginBottom: 15,
-      textAlign: "center"
-    },
-    button: {
-      borderRadius: 20,
-      padding: 10,
-      elevation: 2
     },
 })
 
