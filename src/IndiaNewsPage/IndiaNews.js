2d17247fd72b1584c2224be144e4839ca7728fc1
maazshaikh1711 <0maaz0shaikh0@gmail.com>
2022-01-16 18:28:27 +0530

    renamed "ForYouNews" to "IndiaNews"

--- src/IndiaNewsPage/IndiaNews.js
+++ src/IndiaNewsPage/IndiaNews.js
@@ -3,13 +3,16 @@ import {StyleSheet, Text, View} from 'react-native';
 import {Header} from '../common/components/Header';
 import CardNews from '../CardNews';
 
-const ForYouNews = () => {
+const IndiaNews = () => {
+
+    const URL = 'https://newsapi.org/v2/top-headlines?country=in&apiKey='
+
     return(
         <>
             {/* <Header title={"Rapid News"}/> */}
-            <CardNews/>
+            <CardNews URL={URL}/>
         </>
     )
 }
 
-export default ForYouNews;
+export default IndiaNews;
